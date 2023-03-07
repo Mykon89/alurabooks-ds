391c188a2118d58bc0e5cab2cd79a6234f026948
Mykon <helvis89@gmail.com>
2023-03-07 17:27:52 -0300

    iniciando o projeto de DS

--- stories/AbBotao.stories.tsx
+++ stories/AbBotao.stories.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { AbBotao } from '../src';
 
+// objeto
 export default {
   title: 'Componentes/AbBotao',
   component: AbBotao,
 } as ComponentMeta<typeof AbBotao>;
-
+// retorna o botão renderizado
 const Template: ComponentStory<typeof AbBotao> = () => <AbBotao />;
 
 export const Principal = Template.bind({});
